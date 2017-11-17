93c7541ffecbc9f6f6e79a0a9c127e25e6769f7f
Ross Singer <rossfsinger@gmail.com>
2017-11-17 14:10:50 -0500

    Use correct method signature

--- lib/ResqueScheduler.php
+++ lib/ResqueScheduler.php
@@ -225,7 +225,7 @@ class ResqueScheduler
 			$at = self::getTimestamp($at);
 		}
 	
-		$items = Resque::redis()->zrangebyscore('delayed_queue_schedule', '-inf', $at, array('limit' => array(0, 1)));
+		$items = Resque::redis()->zrangebyscore('delayed_queue_schedule', '-inf', $at);
 		if (!empty($items)) {
 			return $items[0];
 		}
